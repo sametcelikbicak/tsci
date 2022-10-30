d0a4072d687541326df110abc937f94a15cbef13
Samet ÇELİKBIÇAK <sametcelikbicak@outlook.com>
2022-10-30 17:49:29 +0300

    Change configuration (#20)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,15 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    // ...
+    exclude: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/cypress/**",
+      "**/.{idea,git,cache,output,temp}/**",
+      "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress}.config.*",
+      "./lib",
+    ],
   },
-})
+});
